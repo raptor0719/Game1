94470f235aa09a108df8ccae9439fbc5065b83de
raptor0719 <shortman_gamer@hotmail.com>
2020-02-12 17:44:49 -0500

    Make some improvements to Triangle implementation
    - Don't instantiate new objects everytime we want check point contains or want lines
    - Make area calculation static because it could be useful elsewhere

--- src/main/raptor/engine/util/geometry/Triangle.java
+++ src/main/raptor/engine/util/geometry/Triangle.java
@@ -2,16 +2,21 @@ package raptor.engine.util.geometry;
 
 public class Triangle {
 	private final Point[] points;
+	private final LineSegment[] lines;
 	private final double area;
 
 	public Triangle(final Point p0, final Point p1, final Point p2) {
 		points = new Point[3];
-
 		points[0] = p0;
 		points[1] = p1;
 		points[2] = p2;
 
-		this.area = calcArea(p0, p1, p2);
+		lines = new LineSegment[3];
+		lines[0] = new LineSegment(points[0], points[1]);
+		lines[1] = new LineSegment(points[1], points[2]);
+		lines[2] = new LineSegment(points[2], points[0]);
+
+		this.area = calculateArea(p0, p1, p2);
 	}
 
 	public Point[] getPoints() {
@@ -21,11 +26,9 @@ public class Triangle {
 	}
 
 	public LineSegment[] getLines() {
-		final LineSegment[] lines = new LineSegment[3];
-		lines[0] = new LineSegment(points[0], points[1]);
-		lines[1] = new LineSegment(points[1], points[2]);
-		lines[2] = new LineSegment(points[2], points[0]);
-		return lines;
+		final LineSegment[] tmp = new LineSegment[3];
+		System.arraycopy(lines, 0, tmp, 0, 3);
+		return tmp;
 	}
 
 	public double getArea() {
@@ -40,10 +43,10 @@ public class Triangle {
 	}
 
 	public boolean containsPoint(final Point p) {
-		final Triangle t1 = new Triangle(getPoints()[0], getPoints()[1], p);
-		final Triangle t2 = new Triangle(getPoints()[1], getPoints()[2], p);
-		final Triangle t3 = new Triangle(getPoints()[2], getPoints()[0], p);
-		final double area = t1.getArea() + t2.getArea() + t3.getArea();
+		final double area1 = Triangle.calculateArea(getPoints()[0], getPoints()[1], p);
+		final double area2 = Triangle.calculateArea(getPoints()[1], getPoints()[2], p);
+		final double area3 = Triangle.calculateArea(getPoints()[2], getPoints()[0], p);
+		final double area = area1 + area2 + area3;
 		return getArea() == area;
 	}
 
@@ -52,9 +55,9 @@ public class Triangle {
 		return "Triangle:[p0=" + points[0] + ",p1=" + points[1] + ",p2=" + points[2] + "]";
 	}
 
-	/* INTERNALS */
+	/* STATIC */
 
-	private double calcArea(final Point a, final Point b, final Point c) {
+	public static double calculateArea(final Point a, final Point b, final Point c) {
 		final int term1 = a.getX() * (b.getY() - c.getY());
 		final int term2 = b.getX() * (c.getY() - a.getY());
 		final int term3 = c.getX() * (a.getY() - b.getY());
