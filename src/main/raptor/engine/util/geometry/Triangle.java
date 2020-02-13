943d5b05f1cebc66f41836fe4948cac21059b8d1
raptor0719 <shortman_gamer@hotmail.com>
2020-02-12 22:19:47 -0500

    Add static methods using primitives

--- src/main/raptor/engine/util/geometry/Triangle.java
+++ src/main/raptor/engine/util/geometry/Triangle.java
@@ -57,10 +57,24 @@ public class Triangle {
 
 	/* STATIC */
 
+	public static boolean containsPoint(final int t1x, final int t1y, final int t2x, final int t2y, final int t3x, final int t3y, final int px, final int py) {
+		final double area1 = Triangle.calculateArea(t1x, t1y, t2x, t2y, px, py);
+		final double area2 = Triangle.calculateArea(t2x, t2y, t3x, t3y, px, py);
+		final double area3 = Triangle.calculateArea(t3x, t3y, t1x, t1y, px, py);
+		return Triangle.calculateArea(t1x, t1y, t2x, t2y, t3x, t3y) == (area1 + area2 + area3);
+	}
+
 	public static double calculateArea(final Point a, final Point b, final Point c) {
 		final int term1 = a.getX() * (b.getY() - c.getY());
 		final int term2 = b.getX() * (c.getY() - a.getY());
 		final int term3 = c.getX() * (a.getY() - b.getY());
+		return Math.abs((term1 + term2 + term3)/2D);
+	}
+
+	public static double calculateArea(final int ax, final int ay, final int bx, final int by, final int cx, final int cy) {
+		final int term1 = ax * (by - cy);
+		final int term2 = bx * (cy - ay);
+		final int term3 = cx * (ay - by);
 		return Math.abs((term1 + term2 + term3)/2D);
 	}
 }
