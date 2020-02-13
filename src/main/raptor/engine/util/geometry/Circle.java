35756343bccffb5a1161f474ea30f87967862a8d
raptor0719 <shortman_gamer@hotmail.com>
2020-02-13 08:43:59 -0500

    Initial Circle implemenation

--- src/main/raptor/engine/util/geometry/Circle.java
+++ src/main/raptor/engine/util/geometry/Circle.java
@@ -1,18 +1,46 @@
 package raptor.engine.util.geometry;
 
-public class Circle {
-	private int originX;
-	private int originY;
-	private float radius;
+import raptor.engine.util.geometry.api.IPoint;
 
-	public Circle(final int originX, final int originY, final float radius) {
-		this.originX = originX;
-		this.originY = originY;
+public class Circle {
+	private final Point origin;
+	private int radius;
+
+	public Circle(final int originX, final int originY, final int radius) {
+		origin = new Point(originX, originY);
 		this.radius = radius;
 	}
 
 	public boolean isIntersectedByLine(final LineSegment ls) {
-		// TODO
-		return false;
+		return Circle.isIntersectedByLine(origin.getX(), origin.getY(), radius,
+				ls.getPoints().getValue1().getX(), ls.getPoints().getValue1().getY(),
+				ls.getPoints().getValue2().getX(), ls.getPoints().getValue2().getY());
+	}
+
+	public boolean containsPoint(final int px, final int py) {
+		return Circle.containsPoint(origin.getX(), origin.getY(), radius, px, py);
+	}
+
+	public void setOrigin(final int x, final int y) {
+		origin.setX(x);
+		origin.setY(y);
+	}
+
+	public void setRadius(final int radius) {
+		this.radius = radius;
+	}
+
+	public IPoint getOrigin() {
+		return origin;
+	}
+
+	/* STATIC */
+
+	public static boolean isIntersectedByLine(final int originX, final int originY, final int radius, final int l1x, final int l1y, final int l2x, final int l2y) {
+		return Point.distanceTo(originX, originY, l1x, l1y, l2x, l2y) < radius;
+	}
+
+	public static boolean containsPoint(final int originX, final int originY, final int radius, final int px, final int py) {
+		return LineSegment.length(originX, originY, px, py) < radius;
 	}
 }
