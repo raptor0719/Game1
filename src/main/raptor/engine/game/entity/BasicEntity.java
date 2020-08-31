0a5bff1292c3460087fb6da1793d83715ad0702b
raptor0719 <shortman_gamer@hotmail.com>
2020-08-31 19:38:03 -0400

    Set the position to protected to give subclasses access

--- src/main/raptor/engine/game/entity/BasicEntity.java
+++ src/main/raptor/engine/game/entity/BasicEntity.java
@@ -3,7 +3,7 @@ package raptor.engine.game.entity;
 import raptor.engine.util.geometry.Point;
 
 public abstract class BasicEntity implements IEntity {
-	private final Point position;
+	protected final Point position;
 
 	public BasicEntity() {
 		this.position = new Point(0, 0);
