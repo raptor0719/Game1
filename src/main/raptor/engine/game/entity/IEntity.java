2a9ec7a5388762dab632fc75a0b354e601282b58
raptor0719 <shortman_gamer@hotmail.com>
2020-08-31 19:37:45 -0400

    Add draw method as part of api of the entity

--- src/main/raptor/engine/game/entity/IEntity.java
+++ src/main/raptor/engine/game/entity/IEntity.java
@@ -1,7 +1,10 @@
 package raptor.engine.game.entity;
 
+import raptor.engine.display.render.IGraphics;
+
 public interface IEntity {
 	void update();
+	void draw(IGraphics graphics);
 	int getX();
 	int getY();
 }
