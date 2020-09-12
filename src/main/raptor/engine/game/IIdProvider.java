ac02a7bad54d8d86bc9681b23fd0cdb6fbbefa2e
raptor0719 <shortman_gamer@hotmail.com>
2020-09-12 08:07:02 -0400

    Additional methods for IdProvider api

--- src/main/raptor/engine/game/IIdProvider.java
+++ src/main/raptor/engine/game/IIdProvider.java
@@ -2,5 +2,8 @@ package raptor.engine.game;
 
 public interface IIdProvider {
 	long get();
+	long allocate(long request);
 	void free(long id);
+	void freeAll();
+	boolean isFree(long value);
 }
--- src/main/raptor/engine/game/IdProvider.java
+++ src/main/raptor/engine/game/IdProvider.java
@@ -5,24 +5,54 @@ import java.util.Queue;
 
 public class IdProvider implements IIdProvider {
 	private final Queue<Long> available;
-	private long current;
+	private long nextAvailable;
 
 	public IdProvider() {
 		available = new LinkedList<Long>();
-		current = 0;
+		nextAvailable = 0;
 	}
 
 	@Override
 	public long get() {
 		if (!available.isEmpty())
 			return available.poll();
-		final long returnValue = current;
-		current++;
+		final long returnValue = nextAvailable;
+		nextAvailable++;
 		return returnValue;
+	}
+
+	@Override
+	public long allocate(final long request) {
+		if (request < nextAvailable || !available.contains(request))
+			throw new IllegalArgumentException(String.format("Attempted allocation of used id: %s", request));
+
+		if (request == nextAvailable)
+			return get();
+
+		if (available.contains(request)) {
+			available.remove(request);
+			return request;
+		}
+
+		for (long i = nextAvailable; i < request; i++)
+			available.offer(i);
+		nextAvailable = request + 1;
+		return request;
 	}
 
 	@Override
 	public void free(final long id) {
 		available.offer(id);
+	}
+
+	@Override
+	public void freeAll() {
+		available.clear();
+		nextAvailable = 0;
+	}
+
+	@Override
+	public boolean isFree(final long value) {
+		return value >= nextAvailable || available.contains(value);
 	}
 }
