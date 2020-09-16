cd06fe09fdb519c1c61ff9e75dfeb29d051c28f9
raptor0719 <shortman_gamer@hotmail.com>
2020-09-16 18:18:33 -0400

    Add dedicated interface to allow input processing outside of the logical frame timer

--- src/main/raptor/engine/main/Game.java
+++ src/main/raptor/engine/main/Game.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import raptor.engine.display.render.IRenderer;
 import raptor.engine.game.level.Level;
+import raptor.engine.game.ui.input.IMainLoopInputHandler;
 
 public class Game {
 	private static long timeSinceLastFrame;
@@ -18,12 +19,14 @@ public class Game {
 	}
 
 	private final IRenderer renderer;
+	private final IMainLoopInputHandler inputHandler;
 
-	protected Game(final Level initLevel, final IRenderer renderer) {
+	protected Game(final Level initLevel, final IRenderer renderer, final IMainLoopInputHandler inputHandler) {
 		if (currentLevel != null)
 			throw new IllegalStateException("Only 1 instance of the Game is allowed.");
 		currentLevel = initLevel;
 		this.renderer = renderer;
+		this.inputHandler = (inputHandler == null) ? new NoopInputHandler() : inputHandler;
 	}
 
 	public void start() {
@@ -31,6 +34,8 @@ public class Game {
 		while (true) {
 			long currentTime = System.currentTimeMillis();
 			timeSinceLastFrame = currentTime - previousTime;
+
+			inputHandler.handleInputs();
 
 			if (timeSinceLastFrame >= 17) {
 				previousTime = currentTime;
@@ -65,5 +70,14 @@ public class Game {
 
 	public static Object getGlobalProperty(final String propertyName) {
 		return globalProperties.get(propertyName);
+	}
+
+	/* INTERNALS */
+
+	private static class NoopInputHandler implements IMainLoopInputHandler {
+		@Override
+		public void handleInputs() {
+			/* no-op */
+		}
 	}
 }
