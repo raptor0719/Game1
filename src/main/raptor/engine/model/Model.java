ce79f9b04f5413e3771d375240f24616878df3d1
raptor0719 <shortman_gamer@hotmail.com>
2020-09-24 11:45:39 -0400

    Expose some additional properties related to the current frame
    This will allow the dev to act on certain frames. The primary use case would be to tie animations to specific actions, such as spawning a projectile on the first frame of the release part of a bow animation.

--- src/main/raptor/engine/model/Model.java
+++ src/main/raptor/engine/model/Model.java
@@ -26,11 +26,45 @@ public class Model {
 		this.direction = direction;
 	}
 
-	public int advanceFrame() {
+	public void advanceFrame() {
+		currentFrame++;
+	}
+
+	public int remainingFramesInAnimation() {
 		if (currentFrame < 0)
 			return 0;
-		currentFrame++;
 		return currentAnimation.size() - currentFrame - 1;
+	}
+
+	public int getCurrentFrame() {
+		return currentAnimation.get(currentFrame);
+	}
+
+	public boolean isFirstFrameOfCurrentFrame() {
+		if (currentAnimation == null || currentAnimation.size() <= 0)
+			return false;
+		if (currentFrame == 0)
+			return true;
+		return currentAnimation.get(currentFrame - 1) != currentFrame;
+	}
+
+	public boolean isLastFrameOfCurrentFrame() {
+		if (currentAnimation == null || currentAnimation.size() <= 0)
+			return false;
+		if (remainingFramesInAnimation() <= 0)
+			return true;
+		return currentAnimation.get(currentFrame + 1) != currentFrame;
+	}
+
+	public int remainingFramesForCurrentFrame() {
+		if (remainingFramesInAnimation() <= 0)
+			return 0;
+
+		int remaining = 0;
+		for (int i = currentFrame + 1; i < currentAnimation.size() && currentAnimation.get(i) != currentFrame; i++)
+			remaining++;
+
+		return remaining;
 	}
 
 	public HardpointPosition getHardpointPosition(final int hardpoint) {
