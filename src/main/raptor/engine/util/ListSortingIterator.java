4d85cba7a30cff20562c6fd8cab04e97517adc22
raptor0719 <shortman_gamer@hotmail.com>
2020-12-20 13:57:42 -0500

    Widen type

--- src/main/raptor/engine/util/ListSortingIterator.java
+++ src/main/raptor/engine/util/ListSortingIterator.java
@@ -1,6 +1,7 @@
 package raptor.engine.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public class ListSortingIterator<T> implements Iterator<T> {
 	private final Iterator<T> sorted;
 
-	public ListSortingIterator(final List<T> list, final Comparator<T> comparator) {
+	public ListSortingIterator(final Collection<T> list, final Comparator<T> comparator) {
 		final List<T> copy = new ArrayList<>(list);
 		copy.sort(comparator);
 		sorted = copy.iterator();
