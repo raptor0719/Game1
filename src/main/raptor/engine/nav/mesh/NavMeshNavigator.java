f07fa872c4604a3ff5e90b71d9af41df423bf2df
raptor0719 <shortman_gamer@hotmail.com>
2020-12-21 21:30:25 -0500

    Add static methods to construct a nav mesh from simple parts

--- src/main/raptor/engine/nav/mesh/NavMeshNavigator.java
+++ src/main/raptor/engine/nav/mesh/NavMeshNavigator.java
@@ -1,6 +1,7 @@
 package raptor.engine.nav.mesh;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import raptor.engine.nav.api.INavigator;
@@ -8,9 +9,17 @@ import raptor.engine.nav.api.IPathMaterializer;
 import raptor.engine.nav.api.IPointResolver;
 import raptor.engine.nav.api.graph.IGraphNode;
 import raptor.engine.nav.api.graph.IGraphSearch;
+import raptor.engine.nav.mesh.graph.BreadthFirst;
+import raptor.engine.nav.mesh.graph.NavMeshEdge;
 import raptor.engine.nav.mesh.graph.NavMeshNode;
+import raptor.engine.nav.mesh.graph.NavNodeTransformer;
 import raptor.engine.util.ITransformer;
+import raptor.engine.util.ValuePair;
+import raptor.engine.util.geometry.GeometryFunctions;
+import raptor.engine.util.geometry.LineSegment;
 import raptor.engine.util.geometry.Point;
+import raptor.engine.util.geometry.Polygon;
+import raptor.engine.util.geometry.Triangle;
 
 public class NavMeshNavigator implements INavigator {
 	private final IPointResolver<NavMeshNode> navMap;
@@ -47,5 +56,123 @@ public class NavMeshNavigator implements INavigator {
 		final List<Point> cartesianPath = pathMaterializer.materialize(nodePath, start, end);
 
 		return cartesianPath;
+	}
+
+	public static NavMeshNavigator buildNavigator(final Polygon parent, final List<Polygon> holes) {
+		return buildNavigator(parent, holes, GeometryFunctions.fillPolygonWithTriangles(parent, holes));
+	}
+
+	/**
+	* It is assumed the given mesh will be actually valid for the given parent and holes.
+	* Maps are assumed to begin at 0,0
+	*/
+	public static NavMeshNavigator buildNavigator(final Polygon parent, final List<Polygon> holes, final List<Triangle> mesh) {
+		final int cellSize = 100;
+
+		final Point extremus = getXYMaximums(parent);
+		final NavMeshGrid grid = new NavMeshGrid(getCellCount(extremus.getX()), getCellCount(extremus.getY()));
+
+		final List<NavMeshNode> nodes = buildNodes(mesh);
+
+		populateGrid(grid, cellSize, nodes);
+
+		final IPointResolver<NavMeshNode> navMap = new NavMeshPointResolver(grid, getCellCount(extremus.getX()) * cellSize, getCellCount(extremus.getY()) * cellSize);
+
+		final IGraphSearch graphSearch = new BreadthFirst();
+
+		final ITransformer<List<IGraphNode>, List<NavMeshNode>> typeTransformer = new NavNodeTransformer();
+
+		final List<Polygon> areaBounds = new ArrayList<>(holes);
+		areaBounds.add(parent);
+		final IPathMaterializer<NavMeshNode> pathMaterializer = new NavMeshPathMaterializer(areaBounds);
+
+		return new NavMeshNavigator(navMap, graphSearch, typeTransformer, pathMaterializer);
+	}
+
+	private static Point getXYMaximums(final Polygon parent) {
+		final Point maximums = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
+
+		for (final Point p : parent.getPoints()) {
+			maximums.setX(Math.max(p.getX(), maximums.getX()));
+			maximums.setY(Math.max(p.getY(), maximums.getY()));
+		}
+
+		return maximums;
+	}
+
+	private static int getCellCount(final int extreme) {
+		return ((extreme + 99) / 100);
+	}
+
+	private static List<NavMeshNode> buildNodes(final List<Triangle> mesh) {
+		final List<NavMeshNode> nodes = new ArrayList<>();
+
+		int idCounter = 0;
+		for (final Triangle t : mesh)
+			nodes.add(new NavMeshNode(idCounter++, t));
+
+		for (int i = 0; i < nodes.size(); i++) {
+			if (i + 1 >= nodes.size())
+				break;
+
+			final NavMeshNode outer = nodes.get(i);
+			for (int j = i + 1; j < nodes.size(); j++) {
+				final NavMeshNode inner = nodes.get(j);
+
+				buildConnections(outer, inner);
+			}
+		}
+
+		return nodes;
+	}
+
+	// Builds connections FROM start TO end only. Does not modify end at all.
+	private static void buildConnections(final NavMeshNode start, final NavMeshNode end) {
+		for (final LineSegment startSeg : start.getData().getLines()) {
+			for (final LineSegment endSeg : end.getData().getLines()) {
+				if (startSeg.overlaps(endSeg) && !startSeg.isConnected(endSeg))
+					start.addEdge(new NavMeshEdge(end, new ValuePair<LineSegment, LineSegment>(startSeg, endSeg)));
+			}
+		}
+	}
+
+	private static void populateGrid(final NavMeshGrid grid, final int cellSize, final List<NavMeshNode> nodes) {
+		for (int x = 0; x < grid.getXCellCount(); x++) {
+			for (int y = 0; y < grid.getYCellCount(); y++) {
+				final NavMeshGridCell cell = grid.getCell(x, y);
+
+				for (final NavMeshNode node : nodes)
+					if (cellContainsNode(cell, cellSize, node))
+						cell.addNode(node);
+			}
+		}
+	}
+
+	private static boolean cellContainsNode(final NavMeshGridCell cell, final int cellSize, final NavMeshNode node) {
+		final int xAnchor = cell.getX() * cellSize;
+		final int yAnchor = cell.getY() * cellSize;
+
+		// We create points 1 unit shorter on all sides, so that we can use intersection methods for contains
+		final Point cellTopLeft = new Point(xAnchor + 1, yAnchor + 1);
+		final Point cellTopRight = new Point(xAnchor + cellSize - 1, yAnchor + 1);
+		final Point cellBottomRight = new Point(xAnchor + cellSize - 1, yAnchor + cellSize - 1);
+		final Point cellBottomLeft = new Point(xAnchor + 1, yAnchor + cellSize - 1);
+
+		final Polygon cellPoly = new Polygon(Arrays.asList(new Point[] {cellTopLeft, cellTopRight, cellBottomRight, cellBottomLeft}));
+
+		final Triangle nodeTriangle = node.getData();
+
+		// If the cell contains a point of the triangle we know for sure it's in there
+		for (final Point p : nodeTriangle.getPoints())
+			if (cellPoly.containsPoint(p.getX(), p.getY()))
+				return true;
+
+		// If the triangle intersects the cell poly than we know it's inside
+		for (final LineSegment nodeSegment : nodeTriangle.getLines())
+			for (final LineSegment cellSegment : cellPoly.getLines())
+				if (cellSegment.intersectsWith(nodeSegment))
+					return true;
+
+		return false;
 	}
 }
