4c6d15ae37c447c06bc24f4c244c907971eb331c
raptor0719 <shortman_gamer@hotmail.com>
2021-01-11 21:19:47 -0500

    Add methods on ui anchor points to translate given points to the correct location

--- src/main/raptor/engine/ui/UIAnchorPoint.java
+++ src/main/raptor/engine/ui/UIAnchorPoint.java
@@ -1,13 +1,97 @@
 package raptor.engine.ui;
 
 public enum UIAnchorPoint {
-	TOP_LEFT,
-	TOP_CENTER,
-	TOP_RIGHT,
-	CENTER_LEFT,
-	CENTER_RIGHT,
-	BOTTOM_LEFT,
-	BOTTOM_CENTER,
-	BOTTOM_RIGHT,
-	CENTER
+	TOP_LEFT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y;
+		}
+	},
+	TOP_CENTER {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - (width/2);
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y;
+		}
+	},
+	TOP_RIGHT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - width;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y;
+		}
+	},
+	CENTER_LEFT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y - (height/2);
+		}
+	},
+	CENTER_RIGHT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - width;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y - (height/2);
+		}
+	},
+	BOTTOM_LEFT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return  y - height;
+		}
+	},
+	BOTTOM_CENTER {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - (width/2);
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y - height;
+		}
+	},
+	BOTTOM_RIGHT {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - width;
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y - height;
+		}
+	},
+	CENTER {
+		@Override
+		public int translateX(final int x, final int width) {
+			return x - (width/2);
+		}
+		@Override
+		public int translateY(final int y, final int height) {
+			return y - (height/2);
+		}
+	};
+
+	public abstract int translateX(final int x, final int width);
+	public abstract int translateY(final int y, final int height);
 }
