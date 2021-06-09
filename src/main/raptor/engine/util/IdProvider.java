161135c2cd9f857444ffaf1778e0e28fa3d41816
raptor0719 <shortman_gamer@hotmail.com>
2021-06-08 22:00:11 -0400

    Use a HashSet to track available ids for performance increase
    I'm mostly worried about checking the available list, especially when it gets big. This sacrifices memory for speed.

--- src/main/raptor/engine/util/IdProvider.java
+++ src/main/raptor/engine/util/IdProvider.java
@@ -1,21 +1,28 @@
 package raptor.engine.util;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class IdProvider implements IIdProvider {
 	private final Queue<Long> available;
+	private final Set<Long> availableSet;
 	private long nextAvailable;
 
 	public IdProvider() {
 		available = new LinkedList<Long>();
+		availableSet = new HashSet<Long>();
 		nextAvailable = 0;
 	}
 
 	@Override
 	public long get() {
-		if (!available.isEmpty())
-			return available.poll();
+		if (!availableSet.isEmpty()) {
+			final Long id = available.poll();
+			availableSet.remove(id);
+			return id;
+		}
 		final long returnValue = nextAvailable;
 		nextAvailable++;
 		return returnValue;
@@ -23,38 +30,43 @@ public class IdProvider implements IIdProvider {
 
 	@Override
 	public long allocate(final long request) {
-		if (request < nextAvailable && !available.contains(request))
+		if (request < nextAvailable && !availableSet.contains(request))
 			throw new IllegalArgumentException(String.format("Attempted allocation of used id: %s", request));
 
 		if (request == nextAvailable)
 			return get();
 
-		if (available.contains(request)) {
+		if (availableSet.contains(request)) {
+			availableSet.remove(request);
 			available.remove(request);
 			return request;
 		}
 
-		for (long i = nextAvailable; i < request; i++)
+		for (long i = nextAvailable; i < request; i++) {
+			availableSet.add(i);
 			available.offer(i);
+		}
 		nextAvailable = request + 1;
 		return request;
 	}
 
 	@Override
 	public void free(final long id) {
-		if (available.contains(id))
+		if (availableSet.contains(id))
 			return;
+		availableSet.add(id);
 		available.offer(id);
 	}
 
 	@Override
 	public void freeAll() {
+		availableSet.clear();
 		available.clear();
 		nextAvailable = 0;
 	}
 
 	@Override
 	public boolean isFree(final long value) {
-		return value >= nextAvailable || available.contains(value);
+		return value >= nextAvailable || availableSet.contains(value);
 	}
 }
