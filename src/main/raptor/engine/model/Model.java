61ae77ddf01a3dfef9ef38a4a06bf71a72271fe3
raptor0719 <shortman_gamer@hotmail.com>
2021-06-09 23:01:04 -0400

    Remove visual stuff from Model
    It makes way more sense to do this on a level that is aware of global positioning (probably at the Entity level). That way it can calculate this stuff when it needs to, rather than needing to orchestrate an input and output to the system.

--- src/main/raptor/engine/model/Model.java
+++ src/main/raptor/engine/model/Model.java
@@ -1,12 +1,6 @@
 package raptor.engine.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import raptor.engine.util.IIdProvider;
-import raptor.engine.util.IdProvider;
 
 public class Model {
 	private final WireModel wireModel;
@@ -15,25 +9,10 @@ public class Model {
 	private int currentFrame;
 	private Direction direction;
 
-	// Maps a given sprite to a direction and hardpoint index
-	private final DirectionalObjectManager<Sprite> spriteManager;
-
-	private final IIdProvider attachmentIdProvider;
-	private final Map<Long, Attachment> attachments;
-
-	private final List<Sprite> drawList;
-
 	public Model(final WireModel wireModel, Map<Direction, Sprite[]> sprites) {
 		this.wireModel = wireModel;
 		this.hardpointCount = wireModel.getHardpointCount();
 		this.direction = Direction.NORTH;
-
-		this.spriteManager = new DirectionalObjectManager<>(sprites);
-
-		this.attachmentIdProvider = new IdProvider();
-		this.attachments = new HashMap<>();
-
-		this.drawList = new ArrayList<>();
 	}
 
 	public void setFrame(final int frameId) {
@@ -44,86 +23,11 @@ public class Model {
 		this.direction = direction;
 	}
 
-	public void setSprite(final Sprite sprite, final int hardpoint, final Direction direction) {
-		spriteManager.set(sprite, hardpoint, direction);
-	}
-
 	public WireModelFrame getCurrentFrame() {
 		return wireModel.getFrame(currentFrame, direction);
 	}
 
 	public int getHardpointCount() {
 		return hardpointCount;
-	}
-
-	public long attach(final Sprite sprite, final int hardpoint, final int depthModifier) {
-		final long id = attachmentIdProvider.get();
-
-		attachments.put(id, new Attachment(id, sprite, hardpoint, depthModifier));
-
-		return id;
-	}
-
-	public void unattach(final long id) {
-		attachments.remove(id);
-		attachmentIdProvider.free(id);
-	}
-
-	private void recalculateDrawList() {
-		drawList.clear();
-
-		// FIXME: Fill this out
-	}
-
-	private static class Attachment {
-		private final long id;
-		private final Sprite sprite;
-		private final int hardpointIndex;
-		private final int depthModifier;
-
-		public Attachment(final long id, final Sprite sprite, final int hardpointIndex, final int depthModifier) {
-			this.id = id;
-			this.sprite = sprite;
-			this.hardpointIndex = hardpointIndex;
-			this.depthModifier = depthModifier;
-		}
-
-		public long getId() {
-			return id;
-		}
-
-		public Sprite getSprite() {
-			return sprite;
-		}
-
-		public int getHardpointIndex() {
-			return hardpointIndex;
-		}
-
-		public int getDepthModifier() {
-			return depthModifier;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + (int) (id ^ (id >>> 32));
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Attachment other = (Attachment) obj;
-			if (id != other.id)
-				return false;
-			return true;
-		}
 	}
 }
