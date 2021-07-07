ca15042996671d746b9e699117f53b8367a8690a
raptor0719 <shortman_gamer@hotmail.com>
2021-07-07 19:32:22 -0400

    Don't draw if it won't be in the viewport

--- src/main/raptor/engine/display/render/ToViewportGraphicsWrapper.java
+++ src/main/raptor/engine/display/render/ToViewportGraphicsWrapper.java
@@ -5,24 +5,48 @@ import java.awt.Image;
 public class ToViewportGraphicsWrapper implements IGraphics {
 	private final IGraphics graphics;
 	private final LocationToViewportTransformer toViewport;
+	private final Viewport viewport;
 
 	public ToViewportGraphicsWrapper(final IGraphics graphics, final Viewport viewport) {
 		this.graphics = graphics;
 		this.toViewport = new LocationToViewportTransformer(viewport, 1);
+		this.viewport = viewport;
 	}
 
 	@Override
 	public void drawImage(final Image image, final int x, final int y) {
-		graphics.drawImage(image, toViewport.transformX(x), toViewport.transformY(y));
+		final int viewportX = toViewport.transformX(x);
+		final int viewportY = toViewport.transformY(y);
+
+		if (!isInViewport(viewportX, viewportY, image.getWidth(null), image.getHeight(null)))
+			return;
+
+		graphics.drawImage(image, viewportX, viewportY);
 	}
 
 	@Override
 	public void drawOval(final int x, final int y, final int width, final int height, final IColor color) {
-		graphics.drawOval(toViewport.transformX(x), toViewport.transformY(y), width, height, color);
+		final int viewportX = toViewport.transformX(x);
+		final int viewportY = toViewport.transformY(y);
+
+		if (!isInViewport(viewportX, viewportY, width, height))
+			return;
+
+		graphics.drawOval(viewportX, viewportY, width, height, color);
 	}
 
 	@Override
 	public void drawRectangle(final int x, final int y, final int width, final int height, final IColor color) {
-		graphics.drawRectangle(toViewport.transformX(x), toViewport.transformY(y), width, height, color);
+		final int viewportX = toViewport.transformX(x);
+		final int viewportY = toViewport.transformY(y);
+
+		if (!isInViewport(viewportX, viewportY, width, height))
+			return;
+
+		graphics.drawRectangle(viewportX, viewportY, width, height, color);
+	}
+
+	private boolean isInViewport(final int viewportX, final int viewportY, final int width, final int height) {
+		return (viewportX < viewport.getWidth()) && (viewportX + width > 0) && (viewportY < viewport.getHeight()) && (viewportY + height > 0);
 	}
 }
