c217c16a1873828d76287b77578b482927ae3476
raptor0719 <shortman_gamer@hotmail.com>
2022-06-03 10:38:54 -0400

    Add name field to SpriteCollection

--- src/main/raptor/engine/model/SpriteCollection.java
+++ src/main/raptor/engine/model/SpriteCollection.java
@@ -6,15 +6,22 @@ import java.util.Map;
 
 public class SpriteCollection {
 	private final Map<String, DirectionalSprite> phases;
+	private final String name;
 
-	public SpriteCollection(final List<DirectionalSprite> phases) {
+	public SpriteCollection(final String name, final List<DirectionalSprite> phases) {
 		this.phases = new HashMap<>();
 
 		for (final DirectionalSprite sprite : phases)
 			this.phases.put(sprite.getName(), sprite);
+
+		this.name = name;
 	}
 
 	public DirectionalSprite getSprite(final String phase) {
 		return phases.get(phase);
+	}
+
+	public String getName() {
+		return name;
 	}
 }
