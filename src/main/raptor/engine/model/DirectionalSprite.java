98a110047a641ba828c4cc5db7132a688ca58763
raptor0719 <shortman_gamer@hotmail.com>
2022-06-03 11:17:08 -0400

    Remove name field from DirectionalSprite

--- src/main/raptor/engine/model/DirectionalSprite.java
+++ src/main/raptor/engine/model/DirectionalSprite.java
@@ -3,7 +3,6 @@ package raptor.engine.model;
 import java.util.Map;
 
 public class DirectionalSprite {
-	private final String name;
 	private final Map<Direction, Sprite> sprites;
 
 	public DirectionalSprite(final String name, final Map<Direction, Sprite> sprites) {
@@ -11,40 +10,10 @@ public class DirectionalSprite {
 			if (!sprites.containsKey(d) || sprites.get(d) == null)
 				throw new IllegalArgumentException(String.format("Sprite must contain a sprite for each direction. %s was missing.", d.name()));
 
-		this.name = name;
 		this.sprites = sprites;
 	}
 
 	public Sprite getSprite(final Direction direction) {
 		return sprites.get(direction);
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DirectionalSprite other = (DirectionalSprite) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
 	}
 }
